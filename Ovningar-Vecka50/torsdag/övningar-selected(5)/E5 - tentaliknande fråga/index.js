8bbd188d004fb3d0707b683c6681ad99b930b921
Neo <neogoldahlgren@gmail.com>
2024-12-17 16:43:56 +0100

    trololololololololol

--- Ovningar-Vecka50/torsdag/övningar-selected(5)/E5 - tentaliknande fråga/index.js
+++ Ovningar-Vecka50/torsdag/övningar-selected(5)/E5 - tentaliknande fråga/index.js
@@ -6,3 +6,34 @@ function randomInt (min, max) {
 const FirstNames = ["Guillermo", "Antonio", "Gerard", "Nerea", "Carmen", "Itziar"];
 const LastNames = ["Pérez", "González", "López", "Rodríguez", "Antúnez", "Ramírez"];
 
+function randomNames(n) {
+  if (n > (FirstNames.length * LastNames.length)) {
+    return false;
+  }
+
+  let names = [];
+
+  while (names.length < n) {
+    let randomFirstName = FirstNames[randomInt(0, FirstNames.length - 1)];
+    let randomLastName = LastNames[randomInt(0, LastNames.length - 1)];
+    let fullName = randomFirstName + " " + randomLastName;
+
+
+    let foundDupe = false;
+    for (i=0; i<names.length; i++) {
+      if (names[i] === fullName) {
+        foundDupe = true;
+        break;
+      }
+    }
+
+    if (!foundDupe) {
+      names.push(fullName);
+    }
+  }
+
+
+  return names;
+}
+
+console.log(randomNames(10));
